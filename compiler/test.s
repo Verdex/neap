68a10d9a85fd35e9462d69ca98bffb522e0d6131
David L. Inman <davidlevere@gmail.com>
2019-07-23 23:05:18 -0400

    inc prog

--- compiler/test.s
+++ compiler/test.s
@@ -1,1 +1,29 @@
-blah
+
+mod x.y.z {
+    use a.b.c;
+
+    type a = type_info;
+    type b = c;
+    type d<a> = Con(a, d<a>) | Nil;
+    type j = { a : b, c : d };
+    type k = Con { a : b, c : d } | Nil;
+
+    sig w {
+
+        type a;
+        let z : type_info = expr; 
+        fun x ( a : ta, b : tb, ... ) -> xt;
+        ext y ( a : ta, b : tb, ... ) -> yt;
+    }
+}
+
+mod blah<a> {
+    use blarg<b>;
+    use zap<c> as h;
+
+    fun x<b>( a : fun<b, c> ) -> b {
+        h.some_function(1, 2, 3);
+        return [1, 2, 3].map( < %1 + 1 > );
+    }
+
+}
