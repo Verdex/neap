b27d11eae4af2ba0c3c67dbcdc4729910d323d53
David L. Inman <davidlevere@gmail.com>
2019-08-27 23:40:01 -0400

    inc prog

--- compiler/src/data.rs
+++ compiler/src/data.rs
@@ -55,22 +55,12 @@ pub enum Ast {
 
 #[derive(Debug)]
 #[derive(PartialEq)]
-pub struct Use {
-    names : Vec<String> 
-}
-
-#[derive(Debug)]
-#[derive(PartialEq)]
 pub enum TypeInfo {
     Simple(String),
-    Complex(String, Vec<TypeInfo>),
-}
-
-#[derive(Debug)]
-#[derive(PartialEq)]
-pub struct Var {
-    name : String,
-    type_info : Option<TypeInfo>
+    Abstract { name : String, sig_instance_name : String }, 
+    Struct(String, Vec<TypeInfo>),
+    Union(String, Vec<TypeInfo>),
+    Fun { inputs : Vec<TypeInfo>, output : Option<Box<TypeInfo>> },
 }
 
 #[derive(Debug)]
@@ -89,26 +79,22 @@ pub struct Union {
 
 #[derive(Debug)]
 #[derive(PartialEq)]
-pub struct Let {
-    var : Var,
-    expr : Expr,
-}
-
-#[derive(Debug)]
-#[derive(PartialEq)]
-pub struct Set {
-    var : String,
-    expr : Expr,
-}
-
-#[derive(Debug)]
-#[derive(PartialEq)]
-pub struct Try {
-    expr : Expr,
-}
-
-#[derive(Debug)]
-#[derive(PartialEq)]
 pub enum Expr {
+    Try(Expr),    
+    List(Vec<Expr>),
+    Dot(Vec<Expr>),
+    Var(String),
+    Bool(bool),
+    Str(String),
+    Number(f64),
+    ShortLambda(Expr),
+}
 
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub enum Statement {
+    While { condition : Expr, exprs : Vec<Expr> },
+    Set { var : Expr, expr : Expr },  // dot or var
+    Let { var : String, type_info : Option<TypeInfo>, expr : Expr },
+    Use(Vec<String>),
 }
