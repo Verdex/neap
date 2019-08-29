ad9f1ed3cd396abf33646f352d41cef5a273de35
David L. Inman <davidlevere@gmail.com>
2019-08-28 22:43:46 -0400

    inc prog

--- compiler/src/parser.rs
+++ compiler/src/parser.rs
@@ -1,7 +1,20 @@
 
+use super::data::Token;
 use super::data::Ast;
 
-pub fn parse() -> Ast {
+fn blarg(t : Token) {
+
+}
+
+pub fn parse( lexemes : Vec<Token> ) -> Ast {
+    let mut input = lexemes.into_iter();
+
+    loop {
+        match input.next() {
+            Some(lexeme) => blarg(lexeme),
+            None => break,
+        }
+    }
     Ast::Delete 
 }
 
